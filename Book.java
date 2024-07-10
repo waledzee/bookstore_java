2e5c0c10ead2954c9199a5ec9cbbf9dfd62c4901
waledzee <waledzaki201@gmail.com>
2024-07-10 15:13:32 +0300

    book class

---
+++ Book.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.project1;
+
+/**
+ *
+ * @author m
+ */
+public class Book {
+    private String title;
+    private String author;
+    private String ISBN;
+    private int price;
+    private int quantity;
+    
+    public Book(String title,String author,String ISBN, int price,int quantity)
+    {
+        this.title=title;
+        this.author=author;
+        this.ISBN=ISBN;
+        this.price=price;
+        this.quantity=quantity;
+    }
+    
+    public void set_title(String title)
+    {
+        this.title=title;
+    }
+    
+    public String get_title()
+    {
+        return title;
+    }
+     public void set_author(String author)
+    {
+        this.author=author;
+    }
+     public String get_author()
+    {
+        return author;
+    }
+      public void set_ISBN(String ISBN)
+    {
+        this.ISBN=ISBN;
+    }
+      public String get_ISBN()
+    {
+        return ISBN;
+    }
+       public void set_price(int price)
+    {
+        this.price=price;
+    }
+       public int get_price()
+    {
+        return price;
+    }
+        public void set_quantity(int quantity)
+    {
+        this.quantity=quantity;
+    }
+        public int get_quantity()
+    {
+        return quantity;
+    }
+    
+}
